5608d0652acbb1e2c1a9fe30c013d7383a2c90a3
cmickensj <89092260+cmickensj@users.noreply.github.com>
2022-06-30 02:51:35 -0500

    Add files via upload

---
+++ code.js
@@ -0,0 +1,12 @@
+
+
+document.getElementById('hometown').innerHTML = "Memphis";
+document.getElementById('favorites').innerHTML = "Relaxing and Good Music";
+document.getElementById('nickname').innerHTML = "Coco";
+
+var img = document.createElement("img"); 
+ 
+img.src = "https://scontent.fmem1-1.fna.fbcdn.net/v/t1.18169-9/12507594_10208123007888762_3922871042203678018_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=174925&_nc_ohc=dWiJ_cbWLtEAX_z20sj&_nc_ht=scontent.fmem1-1.fna&oh=00_AT-76PmfPlOD_FqAc9f9rMTtaU8slyCSPBo0KjD6TtNlaQ&oe=62E36FF5"; 
+var src = document.getElementById("x"); 
+ 
+src.appendChild(img); 
